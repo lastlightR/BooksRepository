c9dfdac2df2d5a0cf12652dd44661c7350577a18
Jadee26 <148563969+Jadee26@users.noreply.github.com>
2023-11-07 21:23:54 +0100

    rectificación

--- Proyecto1XMLDOM/src/proyecto1xmldom/AccesoDOM.java
+++ Proyecto1XMLDOM/src/proyecto1xmldom/AccesoDOM.java
@@ -64,7 +64,7 @@ public class AccesoDOM {
             
             Node ntitle = documento.createElement("title");//crea etiquetas
             Node ntitle_text = documento.createTextNode(title);//crea el nodo texto
-            ntitle.appendChild(ntitle_text);//añade el titulo a las etiquetas
+            ntitle.appendChild(ntitle_text);//añade el titulo a las
             Node nauthor = documento.createElement("author");
             Node nauthor_text = documento.createTextNode(author);
             nauthor.appendChild(nauthor_text);
@@ -107,24 +107,29 @@ public class AccesoDOM {
         System.out.println("Buscando el Libro" + title + "para borrarlo");
 
         try {
+//Node root=doc.getFirstChild();
             Node raiz = documento.getDocumentElement();
             NodeList nl1 = documento.getElementsByTagName("title");
             Node n1;
             for (int i = 0; i < nl1.getLength(); i++) {
                 n1 = nl1.item(i);
 
-                if (n1.getNodeType() == Node.ELEMENT_NODE) {
+                if (n1.getNodeType() == Node.ELEMENT_NODE) {//redundante por getElementsByTag, no lo es si buscamos getChildNodes()
 
                     if (n1.getChildNodes().item(0).getNodeValue().equals(title)) {
 
                         System.out.println("Borrando el nodo Libro con id" + title);
 
+//n1.getParentNode().removeChild(n1);
+//borra &lt;Titulo&gt; tit &lt;/Titulo&gt;, pero deja Libro y Autor
                         n1.getParentNode().getParentNode().removeChild(n1.getParentNode());
 
                     }
                 }
             }
             System.out.println("Libro borrado");
+//Guardar el arbol DOM en un nuevo archivo para mantener
+
             return 0;
         } catch (Exception e) {
             System.out.println(e);
@@ -139,15 +144,21 @@ public class AccesoDOM {
         try {
             NodeList nodeList = documento.getElementsByTagName("book");
             int length = nodeList.getLength();
+            Node ultimoLibro = nodeList.item(nodeList.getLength()-1);
+            ultimoId =  ((Element) ultimoLibro).getAttribute("id");
+            
+            /*
             if (length > 0) {
                 Node ultimoLibro = nodeList.item(length - 1);
                 String idStr = ((Element) ultimoLibro).getAttribute("id");
                 ultimoId = idStr;
-            }
+            }*/
+            
         } catch (NumberFormatException e) {
             // Manejar la conversión de String a int si es necesario
             e.printStackTrace();
         }
+
         return ultimoId;
     }
     public void pedirUsuario(){
